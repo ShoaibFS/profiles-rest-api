d75ca87ab8e788f58478f17f75e7c94a1a96f8f2
Shoaib <Shoaib@faisal.me>
2024-12-09 12:46:13 +0500

    final commit after all apis

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -29,3 +29,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serialzes profile feed items"""
+
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id','status_text' ,'created_on', 'user_profile')
+        extra_kwargs = {'user_profile': {'read_only': True}}
